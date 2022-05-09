9e26b503b965aaa80b2b7b3adc5770e19c593ce1
nandeesh-gajula <nandeesh.gajula@contentstack.com>
2022-05-09 18:00:22 +0530

    Add remote backend and variable definition

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "YOUR-UNIQUE-BUCKET-ID"
+    bucket = "circle-ci-backend-nandeesh20220509122242298700000001"
     key    = "terraform/webapp/terraform.tfstate"
     region = "us-east-1"
  }
