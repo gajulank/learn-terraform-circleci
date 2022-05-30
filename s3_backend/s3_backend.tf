33285c5f5543ce3f9af6d2559c920413f76090f4
nandeesh-gajula <nandeesh.gajula@contentstack.com>
2022-05-30 14:24:24 +0530

    change in circle

--- s3_backend/s3_backend.tf
+++ s3_backend/s3_backend.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket_prefix = "circle-ci-backend-"
+  bucket_prefix = "circle-ci-backend-nandeeshg"
   # Enable versioning so we can see the full revision history of our
   # state files
   force_destroy = true
